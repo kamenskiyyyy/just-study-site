9907245194160c9e2697a3c4324bab265349dcc0
Nikolay Kamenev <kamenik1@icloud.com>
2022-07-05 11:39:55 +0300

    fix: FRONTEND_URL dynamic

--- config.ts
+++ config.ts
@@ -1,3 +1,7 @@
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+export let FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:8000";
 export const BACKEND_URL_GRAPHQL = `${BACKEND_URL}/api/graphql`;
+
+if (typeof window !== 'undefined') {
+    const FRONTEND_URL = window.location.hostname;
+}
