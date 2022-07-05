8e4bb36c7135bb1b179c2d02cc5054938d576ec7
Nikolay Kamenev <kamenik1@icloud.com>
2022-07-05 11:28:51 +0300

    feat: initial commit

---
+++ apolloClient.ts
@@ -0,0 +1,9 @@
+import {ApolloClient, InMemoryCache,} from "@apollo/client";
+import {BACKEND_URL_GRAPHQL} from "./config";
+
+const client = new ApolloClient({
+    uri: BACKEND_URL_GRAPHQL,
+    cache: new InMemoryCache(),
+});
+
+export default client;
---
+++ pages/server-sitemap.xml.tsx
@@ -0,0 +1,36 @@
+import {getServerSideSitemap} from "next-sitemap";
+import {GetServerSideProps} from "next";
+import {ISitemapField} from "next-sitemap/dist/@types/interface";
+import client from "../apolloClient";
+import {FRONTEND_URL} from "../config";
+import {gql} from "@apollo/client";
+import format from "date-fns/format";
+
+export const GET_ALL_PAGES = gql`
+  query {
+    pages(where: { statusView: { equals: "show" } }) {
+      slug
+      language
+      lastModification
+    }
+  }
+`;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const {data} = await client.query({
+    query: GET_ALL_PAGES,
+    fetchPolicy: 'no-cache'
+  })
+
+  const fields: ISitemapField[] = data.pages.map((page: { language: any; slug: any; lastModification: unknown; }) => {
+    return {
+      loc: `${FRONTEND_URL}/${page.language}/${page.slug}`,
+      lastmod: format(new Date(page.lastModification as unknown as string), 'yyyy-MM-dd') ,
+    };
+  });
+
+  return getServerSideSitemap(ctx, fields);
+};
+
+export default function Sitemap() {
+}
