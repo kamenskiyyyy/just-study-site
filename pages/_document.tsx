7ba0fd2fd9343e5fb379943f5637fb99f7e6aabf
Nikolay Kamenev <kamenik1@icloud.com>
2022-07-16 21:25:25 +0300

    feat: added google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ import createEmotionServer from '@emotion/server/create-instance';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 import { withFork } from 'effector-next';
+import Script from 'next/script';
 
 const enhance = withFork({ debug: false });
 
@@ -17,9 +18,30 @@ class MyDocument extends Document {
                     <link rel="manifest" href="/site.webmanifest" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <style data-href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"></style>
+                    {process.env.NODE_ENV === 'production' && (
+                        <Script id={'google-tag-manager'} strategy={'afterInteractive'}>
+                            {` (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-TM6WWTK');
+                        `}
+                        </Script>
+                    )}
                     {(this.props as any).emotionStyleTags}
                 </Head>
                 <body>
+                    {process.env.NODE_ENV === 'production' && (
+                        <noscript
+                            dangerouslySetInnerHTML={{
+                                __html: `<iframe
+                            src="https://www.googletagmanager.com/ns.html?id=GTM-TM6WWTK"
+                            height="0"
+                            width="0"
+                            style="display:none;visibility:hidden"></iframe>`
+                            }}
+                        />
+                    )}
                     <Main />
                     <NextScript />
                 </body>
