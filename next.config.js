dc555efa2462042df0ef0c0a22ef4c9288649c27
Nikolay Kamenev <kamenik1@icloud.com>
2022-07-23 22:41:09 +0300

    feat: added sentry

--- next.config.js
+++ next.config.js
@@ -34,8 +34,7 @@ const nextConfiguration = {
     }
 }
 
-const sentryConfig = {ignore: ['node_modules', 'webpack.config.js'],
-    configFile: 'sentry.properties'}
+const sentryConfig = {ignore: ['node_modules', 'webpack.config.js']}
 
 module.exports = withPlugins([
     [enhance], [withSentryConfig, sentryConfig]
